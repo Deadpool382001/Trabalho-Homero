50e02444d3d4211eb285de0465a8f8c8493e6a30
Deadpool382001 <wepoethypo@gmail.com>
2024-11-11 22:53:01 -0300

    Add files via upload

---
+++ código.js
@@ -0,0 +1,7 @@
+function clicar1() {
+    window.open('slide.pdf', '_blank')
+}
+
+function clicar2() {
+    window.alert (`Ana Júlia Carvalho` +"\n"+"\n"+ "Daniela de Paiva"  +"\n"+"\n"+  "Emanuelly dos Santos" +"\n"+"\n"+ "Enzo Andrés Rodrigues"+"\n"+"\n"+"Lara Malta Nitta"+"\n"+"\n"+"Letícia Valvazori dos Santos"+"\n"+"\n"+"Maria Fernanda Sakamoto"+"\n"+"\n"+"Matheus Henrique B."+"\n"+"\n"+"Milena Garcia")
+}
